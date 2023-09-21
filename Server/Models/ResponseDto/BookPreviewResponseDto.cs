826fa1f88aea4bf6c87f67a88aeaefd34a23d6fa
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-09-21 13:40:53 +0200

    Add React on frontend

---
+++ Server/Models/ResponseDto/BookPreviewResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using MyLibrary.Server.Models.Entities;
+using MyLibrary.Server.Models.Enums;
+
+namespace MyLibrary.Server.Models.ResponseDto;
+
+public class BookPreviewResponseDto
+{
+    public string Title { get; set; }
+    
+    public Genre Genre { get; set; }
+    
+    public DateTime PublishDate { get; set; }
+    
+    public Author Author { get; set; }
+}
