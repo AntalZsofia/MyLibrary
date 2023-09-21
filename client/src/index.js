9ae0463d5783e209020050629756ce3b6d289953
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-09-21 15:21:00 +0200

    Add react router and endoints

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 
@@ -15,5 +16,33 @@ const router = createBrowserRouter([
     element: <Layout/>,
     errorElement: <ErrorPage />,
     
-  }
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+    errorElement: <ErrorPage/>,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+    errorElement: <ErrorPage/>,
+  },
+  {
+    path:"/Home",
+    element: <Home />,
+    errorElement: <ErrorPage/>
+  },
+  {
+    path: "/MyBooks",
+    element: <MyBooks />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/AddBook",
+    element: <AddBook />,
+    errorElement: <ErrorPage />,
+
+  },
+  
+
 ])
