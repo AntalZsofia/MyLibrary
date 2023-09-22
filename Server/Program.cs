175412133d5aab9314f4479350259c17e622b727
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-09-22 10:16:12 +0200

    chore: Add EF, Migrations

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using MyLibrary.Server.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+// Entity Framework DbContext Configuration
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ;
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql("Server=localhost;Database=MyLibrary;User id=postgres;Password=0Kakipisifing00"));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
