fd9950e1b5cfa88849a5a143bc6038ec6818f26b
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-10-03 12:54:56 +0200

    Create BookDto and AddToCollectionResult

---
+++ Server/Models/RequestDto/BookDto.cs
@@ -0,0 +1,16 @@
+﻿using MyLibrary.Server.Models.Entities;
+using MyLibrary.Server.Models.Enums;
+using MyLibrary.Server.Models.Result.GoogleSearchResult;
+
+namespace MyLibrary.Server.Models.RequestDto;
+
+public class BookDto
+{
+    public string? Title { get; set; }
+    public Author? Author { get; set; }
+    public DateTime PublishDate { get; set; }
+    public Genre Genre { get; set; }
+    public string? Description { get; set; }
+    public GoogleImageLinks? ImageLinks { get; set; }
+    
+}
---
+++ Server/Models/Result/AddToCollectionResult.cs
@@ -0,0 +1,22 @@
+﻿using MyLibrary.Server.Models.ResponseDto;
+
+namespace MyLibrary.Server.Models.Result;
+
+public class AddToCollectionResult
+{
+    public bool Succeeded { get; set; }
+    public Response Response { get; set; }
+
+    public static AddToCollectionResult Succeed(string message)
+    {
+        var response = new Response() { Message = message };
+        return new AddToCollectionResult() { Succeeded = true, Response = response};
+    }
+
+    public static AddToCollectionResult Failed(string message)
+    {
+        var response = new Response() { Message = message };
+        return new AddToCollectionResult() { Succeeded = false, Response = response};
+
+    }
+}
