2ff92ed6c8dc2881561f53385766b292b3a26f2b
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-10-04 15:20:32 +0200

    Add for genre controller, service, dto

---
+++ Server/Services/GenreService.cs
@@ -0,0 +1,21 @@
+﻿using MyLibrary.Server.Models.Enums;
+using MyLibrary.Server.Models.RequestDto;
+
+namespace MyLibrary.Server.Services;
+
+public class GenreService : IGenreService
+{
+    public async Task<IEnumerable<GenreDto>> GetAllGenresAsync()
+    {
+        var genres = Enum.GetValues(typeof(Genre))
+            .Cast<Genre>()
+            .Select(g => new GenreDto
+            {
+                Id = (int)g,
+                Name = g.ToString()
+            })
+            .ToList();
+
+        return await Task.FromResult(genres);
+    }
+}
---
+++ Server/Services/IGenreService.cs
@@ -0,0 +1,8 @@
+﻿using MyLibrary.Server.Models.RequestDto;
+
+namespace MyLibrary.Server.Services;
+
+public interface IGenreService
+{
+    Task<IEnumerable<GenreDto>> GetAllGenresAsync();
+}
