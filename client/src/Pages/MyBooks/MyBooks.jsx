38d6b1ddc7109b30262733a979ca35cad7180655
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-10-04 15:25:07 +0200

    Add MyBooks component on frontend

--- client/src/Pages/MyBooks/MyBooks.jsx
+++ client/src/Pages/MyBooks/MyBooks.jsx
@@ -1,9 +1,25 @@
 
 import React from 'react';
+import {useEffect, useState } from 'react';
 
 const MyBooks = () => {
+    const [userBooks, setUserBooks ] = useState ([]);
+
+    useEffect(() => {
+fetch('http://localhost:5000/book/allbooks')
+.then((res) => res.json())
+.then((data) => {
+    setUserBooks(data);
+})
+.catch((err) => console.error("Error fetching user books", err));
+    },[]);
+
     return <>
-    My Books
+    <ul>
+        {userBooks.map((book) => (
+            <li key={book.id}>{book.title}</li>
+        ))}
+    </ul>
     </>
 }
 export default MyBooks;
