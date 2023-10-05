79c17bdde639d7998bf4ae19bf548c313e1ca038
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-10-05 21:18:57 +0200

    Add data seeding

--- Server/Models/ApplicationDbContext.cs
+++ Server/Models/ApplicationDbContext.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using MyLibrary.Server.Models.Entities;
+using MyLibrary.Server.Models.Enums;
 
 namespace MyLibrary.Server.Models;
 
@@ -11,7 +13,7 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityR
         : base(options)
     {
     }
-   
+
     public DbSet<Author> Authors { get; set; }
     public DbSet<Book> Books { get; set; }
     public DbSet<ApplicationUser> Users { get; set; }
@@ -34,11 +36,84 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityR
 
         modelBuilder.Entity<Book>()
             .HasIndex(b => b.AuthorId);
-        
+
         modelBuilder.Entity<Book>()
             .HasOne(b => b.User)
             .WithMany(u => u.MyBooks)
             .HasForeignKey(b => b.UserId);
     }
-    
-}
+
+    public static async Task Seed(ApplicationDbContext context, UserManager<ApplicationUser> userManager,
+        RoleManager<IdentityRole<Guid>> roleManager)
+    {
+        // Seed roles
+        if (!roleManager.Roles.Any())
+        {
+            var roles = new List<IdentityRole<Guid>>()
+            {
+                new() { Name = "User" }
+            };
+
+            foreach (var identityRole in roles)
+            {
+                await roleManager.CreateAsync(identityRole);
+            }
+        }
+
+        // Seed users
+            if (!userManager.Users.Any())
+            {
+                var newUsers = new List<ApplicationUser>()
+                {
+                    new() { UserName = "Loci", Email = "loci@gmail.com" },
+                    new() { UserName = "Zsofi", Email = "zsofi@gmail.com" },
+                    new() { UserName = "Bianka", Email = "bianka@gmail.com" }
+                };
+
+                foreach (var newUser in newUsers)
+                {
+                    await userManager.CreateAsync(newUser, "Abcd@1234");
+                    await userManager.AddToRolesAsync(newUser, new string[] { "User" });
+                }
+            }
+            //Seed Authors
+
+            if (!context.Authors.Any())
+            {
+                var defaultAuthor = new Author()
+                {
+                    FirstName = "John",
+                    LastName = "Doe"
+                };
+                context.Authors.Add(defaultAuthor);
+                await context.SaveChangesAsync();
+            }
+
+            //Seed books
+            if (!context.Books.Any())
+            {
+                var author = await context.Authors.FirstOrDefaultAsync();
+                var user = await userManager.Users.FirstOrDefaultAsync();
+
+                //int usersNumber = users.Count;
+
+                var random = new Random();
+
+                var books = new List<Book>()
+                {
+                    new Book()
+                    {
+                        Title = "Meet me in Milan",
+                        Author = author,
+                        Genre = Genre.Mystery,
+                        PublishDate = DateTime.Today.ToUniversalTime(),
+                        User = user
+                    }
+                };
+                context.Books.AddRange(books);
+                await context.SaveChangesAsync();
+            }
+        }
+
+    }
+
