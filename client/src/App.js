7ab5a456bed15d185233cd91ca8eee6bc16454de
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-10-06 13:38:28 +0200

    Update App and index.js on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { Route, 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  RouterProvider } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 
 
-function App() {
-  return (
-  <React.Fragment>
-    <Navbar />
-     </React.Fragment>
-  );
+import { AuthProvider } from './Context/AuthContext';
+
+import Layout from "./Layout/Layout";
+import Home from "./Pages/Home/Home.jsx"
+import Login from "./Pages/Login/Login.jsx";
+import Signup from "./Pages/SignUp/Signup.jsx";
+import MyBooks from "./Pages/MyBooks/MyBooks.jsx";
+import AddBook from "./Pages/AddBook/AddBook.jsx";
+
+//import ErrorPage from "./Pages/ErrorPage/ErrorPage";
+
+
+import "./index.css";
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route index element={< Home />} />
+      <Route path='/mybooks' element={< MyBooks />} />
+      <Route path='/addbook' element={< AddBook />} />
+      <Route path='/login' element={< Login />} />
+      <Route path='/signup' element={< Signup />} />
+    </Route>
+  )
+)
+export default function App(){
+  const [user, setUser] = useState(null)
+
+return(
+  <React.StrictMode>
+    <AuthProvider value={{ user, setUser }}>
+    <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
+)
 }
 
-export default App;
+
+reportWebVitals();
--- client/src/index.js
+++ client/src/index.js
@@ -1,66 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
+import App from './App';
 
-import Layout from "./Layout/Layout";
-import Home from "./Pages/Home/Home.jsx"
-import Login from "./Pages/Login/Login.jsx";
-import Signup from "./Pages/SignUp/Signup.jsx";
-import MyBooks from "./Pages/MyBooks/MyBooks.jsx";
-import AddBook from "./Pages/AddBook/AddBook.jsx";
-
-import ErrorPage from "./Pages/ErrorPage/ErrorPage";
-
-
-import "./index.css";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout/>,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/addbook",
-        element: <AddBook />,
-        errorElement: <ErrorPage />,
-    
-      },
-      {
-        path: "/login",
-        element: <Login/>,
-        errorElement: <ErrorPage/>,
-      },
-      {
-        path: "/signup",
-        element: <Signup />,
-        errorElement: <ErrorPage/>,
-      },
-      {
-        path:"/home",
-        element: <Home />,
-        errorElement: <ErrorPage/>
-      },
-      {
-        path: "/mybooks",
-        element: <MyBooks />,
-        errorElement: <ErrorPage />,
-      },
-    ]
-  },
-  
- 
-  
-
-])
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
-
-
-reportWebVitals();
