20498d55c32d9e50d9b4cd089132d3258b839eb3
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-10-09 14:52:56 +0200

    Add Home component and css on frontend

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import './Home.css';
+import Logo from './../../Icons/logo.jpg'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+    <div className='logo-container'>
+    <img src={Logo} alt='logo' className='logo'></img>
+    </div>
+    <div className='welcome'>
+    <h2>Welcome to MyLibrary App</h2>
+    <div>a book management application</div>
+    </div>
+    </>
   )
 }
