ef4874088562793d0d60d8baaa1797934bacc7c7
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-10-10 15:42:23 +0200

    Update Home component on frontend

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import './Home.css';
 import Logo from './../../Icons/logo.jpg'
+import useAuth from '../../Hooks/useAuth';
 
 export default function Home() {
+  const { user } = useAuth();
+  console.log(user);
   return (
     <>
     <div className='logo-container'>
     <img src={Logo} alt='logo' className='logo'></img>
     </div>
     <div className='welcome'>
-    <h2>Welcome to MyLibrary App</h2>
+    <h2>Welcome { user.username ? user.username : '' } to MyLibrary App</h2>
     <div>a book management application</div>
     </div>
     </>
