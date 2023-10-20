14f4fbe17392a828158915c0902fd13cdb218a75
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-10-20 15:04:49 +0200

    Add SearhBooks component on frontend

---
+++ client/src/Components/SearchBooks/SearchBooks.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import BookCardColl from './../../Components/BookCardColl/BookCardColl.jsx';
+
+function SearchBooks({ searchQuery }) {
+  const [searchResults, setSearchResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://localhost:7276/search-book/?query=${searchQuery}`, {credentials: "include"})
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setSearchResults(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+      });
+  }, [searchQuery]);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {searchResults.map((book) => (
+            <BookCardColl
+              key={book.id}
+              id={book.id}
+              title={book.title}
+              author={book.author.name}
+              genre={book.genre}
+              publishYear={book.publishDate}
+              description={book.description}
+              imageUrl={book.smallCoverImage}
+            />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default SearchBooks;
