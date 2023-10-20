094beb3b52d28079fb0d835f1eae08cbdfab35ed
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-10-20 15:05:51 +0200

    Add updated MyBooks component for SearchBar and SearchBooks components + NavBar

---
+++ client/src/Layout/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { NavLink } from "react-router-dom";
+import useAuth from '../Hooks/useAuth';
+
+export default function NavBar() {
+  const { user } = useAuth();
+  console.log(user);
+   return (
+    
+     <nav>
+    <ul className="link-list">
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/mybooks">My Books</NavLink></li>
+        <li><NavLink to="/addbook">Add Book</NavLink></li>
+         {!user ? <li className="right-align"><NavLink to="/login">Login</NavLink></li> 
+         : <li className="right-align"><NavLink to="/profile">Profile</NavLink></li>}
+        {user ? <li><NavLink to="/signout">Log Out</NavLink></li> : <li><NavLink to="/signup">Sign Up</NavLink></li>}
+      </ul>
+    </nav>
+    
+  )
+}
--- client/src/Pages/MyBooks/MyBooks.jsx
+++ client/src/Pages/MyBooks/MyBooks.jsx
@@ -2,10 +2,13 @@
 import React, { useEffect, useState } from 'react';
 import './MyBooks.css';
 import BookCardColl from './../../Components/BookCardColl/BookCardColl.jsx'
+import SearchBar from '../../Components/SearchBar/SearchBar';
+import SearchBooks from '../../Components/SearchBooks/SearchBooks';
 
 const MyBooks = () => {
   const [userBooks, setUserBooks] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     fetch('https://localhost:7276/all-books', {credentials: "include"})
@@ -18,28 +21,37 @@ const MyBooks = () => {
       .catch((err) => console.error("Error fetching user books", err));
   }, []);
 
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+  };
+
   if(isLoading){
     <div>Loading...</div>
   }
   return (
     <>
-    <div className="my-books-container">
-        My Collection</div>
-      <div className="my-books-list">
-        {userBooks.map((book, index) => (
-          <BookCardColl key={index}
-          id={book.id}
-          title={book.title}
-          author={book.author.name}
-          genre={book.genre}
-          publishYear={book.publishDate}
-          description={book.description}
-          imageUrl={book.smallCoverImage} />
-        ))}
-      </div>
+      <div className="my-books-container">My Collection</div>
+      <SearchBar onSearch={handleSearch} />
+      {searchQuery ? (
+        <SearchBooks searchQuery={searchQuery} />
+      ) : (
+        <div className="my-books-list">
+          {userBooks.map((book, index) => (
+            <BookCardColl
+              key={index}
+              id={book.id}
+              title={book.title}
+              author={book.author.name}
+              genre={book.genre}
+              publishYear={book.publishDate}
+              description={book.description}
+              imageUrl={book.smallCoverImage}
+            />
+          ))}
+        </div>
+      )}
     </>
-    
   );
-};
+}
 
 export default MyBooks;
