e342931cc21ffd6f3824e75fc3407f798dfee11d
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-10-25 21:33:28 +0200

    Add UserProfileDto, UpdateProfileDto and UpdateProfileResult

--- MyLib/MyLib/Models/ResponseDto/UserProfileDto.cs
+++ MyLib/MyLib/Models/ResponseDto/UserProfileDto.cs
@@ -5,4 +5,6 @@ public class UserProfileDto
     public string Username { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
+    public int BooksCount { get; set; }
+   
 }
---
+++ MyLib/MyLib/Models/Result/UpdateProfileResult.cs
@@ -0,0 +1,18 @@
+﻿namespace MyLib.Models.Result;
+
+public class UpdateProfileResult
+{
+    public bool Succeeded { get; private set; }
+    
+    public string Message { get; set; } = string.Empty;
+    
+
+    public static UpdateProfileResult Success()
+    {
+        return new UpdateProfileResult() { Succeeded = true, Message = "Profile successfully updated."};
+    }
+    public static UpdateProfileResult Fail()
+    {
+        return new UpdateProfileResult() { Succeeded = false, Message = "Profile with given id doesnt exists."};
+    }
+}
