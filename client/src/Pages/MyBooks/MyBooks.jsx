f4c44675b6573355c83fd5f5428586aea815efc8
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-10-26 20:00:51 +0200

    Update MyBooks component

--- client/src/Pages/MyBooks/MyBooks.jsx
+++ client/src/Pages/MyBooks/MyBooks.jsx
@@ -9,8 +9,10 @@ const MyBooks = () => {
   const [userBooks, setUserBooks] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [searchQuery, setSearchQuery] = useState('');
+  const userBooksCount = userBooks.length;
 
   useEffect(() => {
+    setIsLoading(true);
     fetch('https://localhost:7276/all-books', {credentials: "include"})
       .then((res) => res.json())
       .then((data) => {
@@ -29,28 +31,32 @@ const MyBooks = () => {
     <div>Loading...</div>
   }
   return (
-    <>
+    <div>
       <div className="my-books-container">My Collection</div>
       <SearchBar onSearch={handleSearch} />
       {searchQuery ? (
         <SearchBooks searchQuery={searchQuery} />
       ) : (
-        <div className="my-books-list">
-          {userBooks.map((book, index) => (
-            <BookCardColl
-              key={index}
-              id={book.id}
-              title={book.title}
-              author={book.author.name}
-              genre={book.genre}
-              publishYear={book.publishDate}
-              description={book.description}
-              imageUrl={book.smallCoverImage}
-            />
-          ))}
-        </div>
+        userBooksCount === 0 ? (
+          <div>You don't have any books in your collection.</div>
+        ) : (
+          <div className="my-books-list">
+            {userBooks.map((book, index) => (
+              <BookCardColl
+                key={index}
+                id={book.id}
+                title={book.title}
+                author={book.author.name}
+                genre={book.genre}
+                publishYear={book.publishDate}
+                description={book.description}
+                imageUrl={book.smallCoverImage}
+              />
+            ))}
+          </div>
+        )
       )}
-    </>
+    </div>
   );
 }
 
