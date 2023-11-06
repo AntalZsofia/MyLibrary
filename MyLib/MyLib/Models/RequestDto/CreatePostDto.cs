fdd92e0ae751e41d5243e4788da3365f094182f9
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-11-06 14:10:07 +0100

    Create dto and result for forum feature

---
+++ MyLib/MyLib/Models/RequestDto/CreatePostDto.cs
@@ -0,0 +1,10 @@
+﻿using MyLib.Models.Entities;
+
+namespace MyLib.Models.RequestDto;
+
+public class CreatePostDto
+{
+    public string Content { get; set; }
+    public DiscussionThread DiscussionThread { get; set; }
+    public DateTime PostCreationDate { get; set; }
+}
---
+++ MyLib/MyLib/Models/Result/ForumActionResult.cs
@@ -0,0 +1,22 @@
+﻿using MyLib.Models.ResponseDto;
+
+namespace MyLib.Models.Result;
+
+public class ForumActionResult
+{
+    public bool Succeeded { get; set; }
+    public Response Response { get; set; }
+
+    public static ForumActionResult Succeed(string message)
+    {
+        var response = new Response() { Message = message };
+        return new ForumActionResult() { Succeeded = true, Response = response};
+    }
+
+    public static ForumActionResult Failed(string message)
+    {
+        var response = new Response() { Message = message };
+        return new ForumActionResult() { Succeeded = false, Response = response};
+
+    }
+}
