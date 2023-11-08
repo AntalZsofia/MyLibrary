baac4cedfcabad0496b96a1b967f9c12603bb92c
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-11-08 14:41:05 +0100

    Update PostCard component for frontend (selected post)

--- client/src/Components/PostCard/PostCard.jsx
+++ client/src/Components/PostCard/PostCard.jsx
@@ -1,32 +1,54 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 //import { NavLink } from "react-router-dom";
 import './PostCard.css';
 import ReplyCard from '../ReplyCard/ReplyCard';
+import { useNavigate, useParams } from 'react-router';
 
 
-const PostCard = ({ postTitle, content, postCreationDate, user }) => {
+const PostCard = ({ postTitle, content, postCreationDate, user, id }) => {
   const [replying, setReplying] = useState(false);
-
+  const [post, setPost] = useState('');
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
+  const { id } = useParams();
+  
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`https://localhost:7276/get-post/${id}`, { credentials: 'include' })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPost(data);
+        setIsLoading(false);
+      })
+      .catch((err) => console.error("Error fetching post", err));
+  }, [])
   const handleClickReply = () =>{
 setReplying(true);
+  }
+
+  const handleCancel = () => {
+    navigate('/forum');
   }
 
     return (
         <div className="post-card-container">
           
           <div className="post-details">
-            <p className="post-postTitle">Title: {postTitle}</p>
-            <p className="post-content">Content: {content}</p>
-            <p className="post-postCreatinDate">Posted at: {postCreationDate}</p>
-            <p className="post-user">By: {user}</p> 
+            <p className="post-postTitle">Title: {post.postTitle}</p>
+            <p className="post-content">Content: {post.content}</p>
+            <p className="post-postCreatinDate">Posted at: {post.postCreationDate}</p>
+            <p className="post-user">By: {post.user.username}</p> 
             
             <div className="post-buttons">
           <button className="post-like-button">Like</button>
           <button className="post-reply-button" onClick={handleClickReply}>Reply</button>
+          <button className="cancel-button" onClick={handleCancel}>Cancel</button>
         </div>
             </div>
             {replying && (
-        <ReplyCard />
+        <ReplyCard
+        id={id} />
       )}
           </div>
       );
