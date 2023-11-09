a16e1cf18cc95e4b5e26eac4f758642a54767b5a
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-11-09 14:58:39 +0100

    Update ReplyCard component

--- client/src/Components/ReplyCard/ReplyCard.jsx
+++ client/src/Components/ReplyCard/ReplyCard.jsx
@@ -1,32 +1,59 @@
 import React, { useState } from 'react';
 import './ReplyCard.css';
+import { useNavigate, useParams } from 'react-router';
 
-const ReplyCard = () => {
+const ReplyCard = ({ onReplySubmit }) => {
   const [replyText, setReplyText] = useState('');
+  const navigate = useNavigate();
+  const { id } = useParams();
 
-  const handleReplyTextChange = (e) => {
-    setReplyText(e.target.value);
+  const handleReplySubmit = async () => {
+    try {
+
+      const newReply = {
+        reply: replyText
+      }
+      const response = await fetch(`https://localhost:7276/create-reply/${id}`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newReply),
+      });
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log('Reply added successfully:', responseData);
+        onReplySubmit();
+        setReplyText('');
+      } else {
+        console.error('Error sending new reply:', response.statusText);
+      }
+    }
+    catch (err) {
+      console.error("Error sending new reply", err);
+    };
   }
 
-  const handleReplySubmit = () => {
-    // Implement your reply submission logic here
-    // You can send the replyText to a server or update the UI as needed
-    // After submitting, you may want to reset the reply form or close it
-  }
   const handleCancel = () => {
-    oncancel();
+    navigate('/forum');
   }
 
   return (
     <div className="reply-form">
+      <h4>New Reply</h4>
       <textarea
+      className='reply-textarea'
         rows="5"
         placeholder="Write your reply"
         value={replyText}
-        onChange={handleReplyTextChange}
+        onChange={(e) => setReplyText(e.target.value)}
       />
-      <button className="submit-reply-button" onClick={handleReplySubmit}>Submit</button>
-      <button className='cancel-button' onClick={handleCancel}>Cancel</button>
+      <div className="submit-cancel-buttons">
+        <button className="submit-reply-button" onClick={handleReplySubmit}>Submit</button>
+        <button className="cancel-button" onClick={handleCancel}>Cancel</button>
+      </div>
+
     </div>
   );
 };
