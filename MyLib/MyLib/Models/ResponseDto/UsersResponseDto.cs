6155f208233c685887fd90d6a613a66923bbc806
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-11-13 11:54:09 +0100

    Add UserResponseDto for the result

---
+++ MyLib/MyLib/Models/ResponseDto/UsersResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MyLib.Models.ResponseDto;
+
+public class UsersResponseDto
+{
+    public DateTime ProfileCreationDate { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    
+
+}
