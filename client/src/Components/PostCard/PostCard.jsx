f55faff4f1835338b3bbb8818e9cee05953f1aed
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-11-17 10:52:59 +0100

    Update PostCard for check user is the postUser

--- client/src/Components/PostCard/PostCard.jsx
+++ client/src/Components/PostCard/PostCard.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import './PostCard.css';
 import ReplyCard from '../ReplyCard/ReplyCard';
-import { useParams } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
 import Replies from '../Replies/Replies';
 import Like from './../../Icons/like.png';
+import useAuth from '../../Hooks/useAuth';
 
 
 export function convertDate(timestamp) {
@@ -25,8 +26,11 @@ const PostCard = () => {
   const [replying, setReplying] = useState(false);
   const [post, setPost] = useState('');
   const [isLoading, setIsLoading] = useState(true);
- const { id } = useParams();
-  
+  const { id } = useParams();
+  const { user } = useAuth();
+  const [postUser, setPostUser] = useState('');
+  const navigate = useNavigate();
+
 
   useEffect(() => {
     setIsLoading(true);
@@ -36,38 +40,48 @@ const PostCard = () => {
         console.log(data);
         setPost(data);
         setIsLoading(false);
+        setPostUser(data.user);
+        console.log("post user: " + postUser.userName);
       })
       .catch((err) => console.error("Error fetching post", err));
   }, [id])
-  
 
- 
-  if(isLoading){
+const handleClick = () => {
+  navigate(`/update-post/${id}`);
+}
+
+  if (isLoading) {
     return <div>Loading...</div>
   }
 
-    return (
-      <>
-        <div className="post-card-container">
-          
-          <div className="post-details">
-            <p className="post-postTitle">Title: {post.discussionThread}</p>
-            <p className="post-content">Content: {post.content}</p>
-            <p className="post-postCreatinDate">Posted at: {convertDate(post.postCreationDate)}</p>
-            <p className="post-user">By: {post.user.userName}</p> 
-            <div className='like-button-container'>
+  return (
+    <>
+      <div className="post-card-container">
+
+        <div className="post-details">
+          <p className="post-postTitle">Title: {post.discussionThread}</p>
+          <p className="post-content">Content: {post.content}</p>
+          <p className="post-postCreatinDate">Posted at: {convertDate(post.postCreationDate)}</p>
+          <p className="post-user">By: {post.user.userName}</p>
+          <div className='like-button-container'>
             <img src={Like} alt='logo' className='like'></img>
             <p className='like-message'>Like</p>
             </div>
+            <div className='post-more-button-container'>
+            {postUser.userName.username === user.userName && (
+              <button className='post-more-button' onClick={handleClick}>More</button>
+            )}
             </div>
-            <div className='reply-posts-container'>
-              <Replies discussionThread={post.discussionThread}/>
-            </div>
-        
-          </div>
-            {replying && (
+      </div>
+      <div className='reply-posts-container'>
+        <Replies discussionThread={post.discussionThread} />
+      </div>
+
+    </div >
+      { replying && (
         <ReplyCard />
-      )}
+      )
+}
       </>
       );
 };
