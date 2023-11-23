51746195573f87c1eee053336494aff8a57dddeb
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-11-23 17:55:54 +0100

    Add Buttons container for BookCard component

--- client/src/Components/BookCard/BookCard.jsx
+++ client/src/Components/BookCard/BookCard.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './BookCard.css';
 
-const BookCard = ({title, author, genre, smallCoverImage, publishDate, description}) => {
+const BookCard = ({ title, author, genre, smallCoverImage, publishDate, description }) => {
   const navigate = useNavigate();
 
   const handleAddBook = async () => {
-    try{
+    try {
       const bookToSave = {
         title,
         author,
@@ -14,46 +14,45 @@ const BookCard = ({title, author, genre, smallCoverImage, publishDate, descripti
         publishDate,
         description,
         smallCoverImage,
-       
+
       }
       const response = await fetch('https://localhost:7276/add-to-collection', {
-          method: 'POST',
-          credentials:'include',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(bookToSave),
-  } );
-  if (response.ok) {
-    const responseData = await response.json();
-    console.log('Book added successfully:', responseData); // Log the response data
-    navigate('/mybooks');
-} else {
-    console.error('Error adding book to collection:', response.statusText);
-}
-}
-  catch(err){
-  console.error("Error adding book to collection", err);
-  }
-};
-  
-return(
-  
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(bookToSave),
+      });
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log('Book added successfully:', responseData); // Log the response data
+        navigate('/mybooks');
+      } else {
+        console.error('Error adding book to collection:', response.statusText);
+      }
+    }
+    catch (err) {
+      console.error("Error adding book to collection", err);
+    }
+  };
 
-  <div className="book-card">
-    <img src={smallCoverImage} alt={`${title} cover`} className="book-image" />
+  return (
 
-    <div className='book-details'>
-    <h2 className="book-title">{title}</h2>
-    <p className="book-author">Author: {author} </p>
-    <p className="book-genre">Genre: {genre}</p>
-    <p className="book-publish-year">Published: {publishDate}</p>
-    <button className="add-book-button" onClick={handleAddBook}>Add</button>
-    
+
+    <div className="book-card">
+      <img src={smallCoverImage} alt={`${title} cover`} className="book-image" />
+
+      <div className='book-details'>
+        <h2 className="book-title">{title}</h2>
+        <p className="book-author">Author: {author} </p>
+        <p className="book-genre">Genre: {genre}</p>
+        <p className="book-publish-year">Published: {publishDate}</p>
+        <div className="book-card-buttons-container">
+          <button className="add-book-button" onClick={handleAddBook}>Add</button>
+        </div>
+      </div>
     </div>
-  </div>
-
- 
-);
+  );
 }
 export default BookCard;
