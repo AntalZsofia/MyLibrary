961f0e46768f87c0ef5680b52710ea99f4fef87d
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-11-23 17:55:24 +0100

    Update BookCardColl with a new container for the image and rating

--- client/src/Components/BookCardColl/BookCardColl.jsx
+++ client/src/Components/BookCardColl/BookCardColl.jsx
@@ -18,14 +18,8 @@ const BookCardColl = ({ title, author, genre, imageUrl, publishYear, description
 
   return (
     <div className="book-card">
+      <div className="image-and-rating-container">
       <img src={imageUrl} alt={`${title} cover`} className="book-image" />
-
-      <div className="book-details">
-        <h2 className="book-title">{title}</h2>
-        <p className="book-author">Author: {author}</p>
-        <p className="book-genre">Genre: {genre}</p>
-        <p className="book-publish-year">Published: {publishYear}</p>
-        <div className="bottom-container">
 
           <div className="rating-container">
             <span className="star-container">
@@ -33,15 +27,23 @@ const BookCardColl = ({ title, author, genre, imageUrl, publishYear, description
               {[...Array(5)].map((e, i) => <Star key={i} selected={i < rating} />)}
             </span>
           </div>
-          <div className='icons-container'>
-          <div className="buttons-container">
-            {readingStatus === 'Reading' && <img src={openBook} alt="Reading" style={{ width: '40px', height: '40px' }} />}
-            {readingStatus === 'Finished' && <img src={checked} alt="Finished" style={{ width: '40px', height: '40px' }} />}
-            <NavLink to={`/selected-book/${id}`}>
-              <button className="more-book-button">More</button>
-            </NavLink>
-</div>
-          </div>
+      </div>
+      <div className="book-details">
+        <h2 className="book-title">{title}</h2>
+        <p className="book-author">Author: {author}</p>
+        <p className="book-genre">Genre: {genre}</p>
+        <p className="book-publish-year">Published: {publishYear}</p>
+        <div className="bottom-container">
+
+          
+            <div className="buttons-container">
+              {readingStatus === 'Reading' && <img src={openBook} alt="Reading" style={{ width: '40px', height: '40px' }} />}
+              {readingStatus === 'Finished' && <img src={checked} alt="Finished" style={{ width: '40px', height: '40px' }} />}
+              <NavLink to={`/selected-book/${id}`}>
+                <button className="more-book-button">More</button>
+              </NavLink>
+            </div>
+       
         </div>
       </div>
 
