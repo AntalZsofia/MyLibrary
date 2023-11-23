bc6b0ab80bed74eaac49fc2e0291d13471fd9ac9
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-11-23 13:12:17 +0100

    Update BookCardColl component to see the rated stars

--- client/src/Components/BookCardColl/BookCardColl.jsx
+++ client/src/Components/BookCardColl/BookCardColl.jsx
@@ -4,11 +4,16 @@ import { NavLink } from "react-router-dom";
 import openBook from './../../Icons/open-book.png';
 import checked from './../../Icons/checked.png';
 
+const Star = ({ selected = false, onClick = f => f }) => (
+  <div onClick={onClick} className={selected ? 'star-selected' : 'star-not-selected'}>
+    {selected ? '★' : '☆'}
+  </div>
+);
 
 
-const BookCardColl = ({ title, author, genre, imageUrl, publishYear, description, id, readingStatus}) => {
- 
-  
+const BookCardColl = ({ title, author, genre, imageUrl, publishYear, description, id, readingStatus, rating }) => {
+
+
 
 
   return (
@@ -20,16 +25,26 @@ const BookCardColl = ({ title, author, genre, imageUrl, publishYear, description
         <p className="book-author">Author: {author}</p>
         <p className="book-genre">Genre: {genre}</p>
         <p className="book-publish-year">Published: {publishYear}</p>
-        <div className="buttons-container">
-        {readingStatus === 'Reading' && <img src={openBook} alt="Reading" style={{width: '40px', height: '40px'}}/>} 
-        {readingStatus === 'Finished' && <img src={checked} alt="Finished" style={{width: '40px', height: '40px'}}/>}
-          <NavLink to={`/selected-book/${id}`}>
-            <button className="more-book-button">More</button>
-          </NavLink>
-         
+        <div className="bottom-container">
+
+          <div className="rating-container">
+            <span className="star-container">
+
+              {[...Array(5)].map((e, i) => <Star key={i} selected={i < rating} />)}
+            </span>
+          </div>
+          <div className='icons-container'>
+          <div className="buttons-container">
+            {readingStatus === 'Reading' && <img src={openBook} alt="Reading" style={{ width: '40px', height: '40px' }} />}
+            {readingStatus === 'Finished' && <img src={checked} alt="Finished" style={{ width: '40px', height: '40px' }} />}
+            <NavLink to={`/selected-book/${id}`}>
+              <button className="more-book-button">More</button>
+            </NavLink>
+</div>
+          </div>
         </div>
       </div>
-     
+
     </div>
   );
 };
