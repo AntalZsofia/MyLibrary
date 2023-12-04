c0f39c21b7eca9b5eed1aa9951ac862777ed89f7
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-12-04 11:42:54 +0100

    Add context for the dark mode

---
+++ client/src/Context/ThemeProvider.jsx
@@ -0,0 +1,18 @@
+import React, { useState, createContext } from 'react';
+
+// Create a context
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
