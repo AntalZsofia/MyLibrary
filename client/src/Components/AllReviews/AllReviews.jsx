ac5d88bdbb4020b1d929873019ef00d174ea37d1
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-12-06 11:13:14 +0100

    Add dark mode to AllReviews component + minor change in Switch component

--- client/src/Components/AllReviews/AllReviews.jsx
+++ client/src/Components/AllReviews/AllReviews.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import './AllReviews.css';
+import { ThemeContext } from '../../Context/ThemeProvider';
+import { useContext } from 'react';
 
 export default function AllReviews() {
     const [reviews, setReviews] = useState([]);
+    const { darkMode } = useContext(ThemeContext);
    
 
 
@@ -30,11 +33,11 @@ export default function AllReviews() {
 
     return (
 
-        <div className='all-reviews-container'>
+        <div className={`all-reviews-container ${darkMode ? 'dark-mode' : ''}`}>
             {!reviews || reviews.length === 0 ? (
                 <div className='no-reviews-message'>There are no reviews to show.</div>
             ) : (
-                <div className="reviews-list">
+                <div className={`reviews-list ${darkMode ? 'dark-mode' : ''}`}>
                     {reviews.map((review, index) => (
                         <div className='review-card'key={review.id || index}>
                             <img src={review.smallCoverImage} alt={review.title} className='review-card-image' />
@@ -42,11 +45,11 @@ export default function AllReviews() {
                                 <div className='review-card-title'>{review.title}</div>
                                 <div className='review-card-author'>by {review.author}</div>
                             </div>
-                            <div className='review-card-rating'>Rating: {review.rating}</div>
-                            <div className='review-card-review'>Review: {review.review}</div>
+                            <div className={`review-card-rating ${darkMode ? 'dark-mode' : ''}`}>Rating: {review.rating}</div>
+                            <div className={`review-card-review ${darkMode ? 'dark-mode' : ''}`}>Review: {review.review}</div>
                             <div className='review-buttons-container'>
-                                <button className='review-card-edit-button'>Edit</button>
-                                <button className='review-card-delete-button'>Delete</button>
+                                <button className={`review-card-edit-button ${darkMode ? 'dark-mode' : ''}`}>Edit</button>
+                                <button className={`review-card-delete-button ${darkMode ? 'dark-mode' : ''}`}>Delete</button>
                             </div>
                         </div>
                     ))}
