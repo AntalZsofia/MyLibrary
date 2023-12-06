92fbc5e54e4d843c940539464fd11d6599ca328b
ZsofiaAntal <antal.zsofia84@gmail.com>
2023-12-06 13:45:00 +0100

    Add minor changes to AddBook and AddBookManual components

--- client/src/Pages/AddBook/AddBook.jsx
+++ client/src/Pages/AddBook/AddBook.jsx
@@ -11,8 +11,8 @@ const AddBook = () => {
     return (
     <>
      <div className={`add-book-links ${darkMode ? 'dark-mode' : ''}`}>
-                <Link to="/addbook" className='link'>Search</Link>
-                <Link to="addbook-manual" className='link'>Manual entry</Link>
+                <Link to="/addbook" className={`link ${darkMode ? 'dark-mode' : ''}`}>Search</Link>
+                <Link to="addbook-manual" className={`link ${darkMode ? 'dark-mode' : ''}`}>Manual entry</Link>
       </div>
                 <Outlet />
       
